507231e858b6c9b436a72aa4e24b68ff8b506dbd
PaperChalice <liujunchang97@outlook.com>
2026-09-11 15:58:18 +0800

    [MMIX] Add some missing inst alias

--- llvm/test/MC/MMIX/alias.s
+++ llvm/test/MC/MMIX/alias.s
@@ -1,22 +1,31 @@
-# RUN: llvm-mc -triple=mmix %s --show-encoding 2>&1 | FileCheck %s
+% RUN: llvm-mc -triple=mmix %s --show-encoding 2>&1 | FileCheck %s
 
-# CHECK: encoding: [0x20,0x00,0x00,0x00]
+% CHECK: encoding: [0x20,0x00,0x00,0x00]
   ADD $0,$0,$0
 
-# CHECK: encoding: [0x21,0x00,0x00,0x01]
+% CHECK: encoding: [0x21,0x00,0x00,0x01]
   ADD $0,$0,1
 
-# CHECK: encoding: [0x21,0x00,0x00,0x01]
+% CHECK: encoding: [0x21,0x00,0x00,0x01]
   ADDI $0,$0,1
 
-# CHECK: encoding: [0x00,0x00,0x07,0x01]
+% CHECK: encoding: [0x00,0x00,0x07,0x01]
   TRAP 0,Fputs,StdOut
 
-# CHECK: encoding: [0x00,0x00,0x07,0x01]
+% CHECK: encoding: [0x00,0x00,0x07,0x01]
   TRAP 0,7,1
 
-# CHECK: SET $0,$1
+% CHECK: SET $0,$1
   OR $0,$1,0
-
-# CHECK: SET $0,1
+% CHECK: SET $0,1
   SET $0,1
+
+% CHECK: LDB $0,$0
+  LDB $0,$0,0
+% CHECK: LDB $0,$0,1
+  LDB $0,$0,1
+
+% CHECK: NEG $0,1
+  NEG $0,0,1
+% CHECK: NEG $0,$1
+  NEG $0,0,$1
