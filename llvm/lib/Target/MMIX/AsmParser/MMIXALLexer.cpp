bc10dc41678d2bf59e08145f34b0a3ec059dcc39
PaperChalice <liujunchang97@outlook.com>
2026-09-16 22:03:21 +0800

    [MMIX] Add MMIXAL parser

---
+++ llvm/lib/Target/MMIX/AsmParser/MMIXALLexer.cpp
@@ -0,0 +1,11 @@
+//===-- MMIXALLexer.cpp - Lex MMIX assembly language ----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "MMIXALLexer.h"
+
+using namespace llvm;
